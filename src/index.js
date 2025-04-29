829beaabe0116ea53d0fbb8d0d4063c2fffe5c46
Daniil <developmentweb15@gmail.com>
2025-04-29 18:23:35 +0300

    Linked Todos to localStorage

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import { createTodo } from "./modules/Todo";
 import { createProject} from "./modules/Project";
 import { displayTodosArray } from "./modules/displayTodo";
 import { displayProjectNames } from "./modules/displayProject";
+import { updateTodoStorage,getTodoFromStorage} from "./modules/localstorage";
 //todos
 
-let todosArray=[];
+let todosArray=getTodoFromStorage();
+
 function addTodoToArray(){
     const name=window.prompt("name");
     const dueDate=window.prompt("dueDate");
@@ -24,7 +26,8 @@ const selectTodos=document.getElementById("todos");
 
 newTodo.addEventListener("click",()=>{
     addTodoToArray();
-    displayTodosArray(todosArray);
+    updateTodoStorage(todosArray);
+    displayTodosArray(getTodoFromStorage());
 })
 
 newProject.addEventListener("click",()=>{
@@ -36,4 +39,5 @@ selectTodos.addEventListener("click",()=>{
     displayTodosArray(todosArray);
     alert("clicked");
 })
+displayTodosArray(todosArray);
 
