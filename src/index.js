82b7522b8ac8423ac169d9a557686d94e54015ec
Daniil <developmentweb15@gmail.com>
2025-05-16 13:48:11 +0300

    i came to idea that this project dont have to be that complex so i will restart it(again) and i will focus more on goodlooking responsible UI than complex app logic

--- src/index.js
+++ src/index.js
@@ -1,15 +1,1 @@
-import { inputProjectName,displayProjectNames, displayTODO } from "./modules/DOM";
 
-displayProjectNames();
-const TODO=document.getElementById("TODO's");
-
-TODO.addEventListener('click',()=>{
-    
-    displayTODO();
-});
-const newProjectBtn=document.getElementById('add-project');
-newProjectBtn.addEventListener('click',()=>{
-    alert('clicked');
-    inputProjectName();
-    displayProjectNames();
-});
